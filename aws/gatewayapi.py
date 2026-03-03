33cd21b4f3a9159f5661abd4222e09533fffd311
Michael Tilelli <38261361+tilellimjx@users.noreply.github.com>
2026-03-03 13:12:49 -0700

    more gateway api py fixes

--- aws/gatewayapi.py
+++ aws/gatewayapi.py
@@ -7,7 +7,7 @@ def lambda_function(event, context):
     print("Received event: " + json.dumps(event, indent=2))
     
     # Create a client for the API Gateway service
-    client = boto3.client('apigateway')
+    #client = boto3.client('apigateway')
 
     # Create a client for the S3 service
     s3_client = boto3.client('s3')
@@ -17,7 +17,8 @@ def lambda_function(event, context):
 
     # try to get the json data from the response and handle any exceptions
     try:
-        json_data = json.dumps(event)
+        json_data = json.dumps(event).encode("utf-8")
+        print(f'type(json_data): {type(json_data)}')
     except (TypeError, ValueError) as e:
         # log the error to cloudwatch logs
         print(f"Error converting data to JSON: {e}")
@@ -25,7 +26,7 @@ def lambda_function(event, context):
         # copy apis to an s3 bucket along with the error message
         data = f'Error converting data to JSON: {e}\n\nData: {event}'
         
-        error_object_key = f'gateway_apis_error_{timestamp}.txt'
+        error_object_key = f'rt_data_error_{timestamp}.txt'
         s3_client.put_object(Bucket=error_bucket_name, Key=error_object_key, Body=data)
         print(f"Error data uploaded to S3 bucket '{error_bucket_name}' with object key '{error_object_key}'")
         
@@ -35,7 +36,7 @@ def lambda_function(event, context):
         }
         
     # Define the bucket name and object key with a timestamp to the ms to avoid overwriting existing data
-    object_key = f'gateway_apis_{timestamp}.json'
+    object_key = f'rt_data_{timestamp}.json'
 
     # Upload the JSON data to the S3 bucket
     s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=json_data)
